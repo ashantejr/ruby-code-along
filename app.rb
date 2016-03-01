5350c36094e164692e4d0905a2a9db77ff853777
ashantejr <ashantejr@gmail.com>
2016-03-01 23:17:01 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,90 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+get "/" do
+  "<style>
+    body{
+      background: purple;
+    }
+    h1{
+      color: white;
+    }
+    h2{
+      color: white;
+    }
+    h3{
+      color: lime;
+    }
+  </style>
+
+  <h1>Everybody Talks </h1>
+  <h2>By The Neon Trees</h2>
+  <h3>
+  Hey, baby, won't you look my way? <br>
+  I can be your new addiction <br>
+  Hey, baby, what you gotta say? <br>
+  All you're giving me is fiction <br>
+
+  I'm a sorry sucker and this happens all the time <br>
+  I found out that everybody talks <br>
+  Everybody talks, everybody talks <br>
+
+  It started with a whisper <br>
+  And that was when I kissed her <br>
+  And then she made my lips hurt <br>
+  I can hear the chitchat <br>
+  Take me to your love shack <br>
+  Mamas always gotta backtrack <br>
+  When everybody talks back <br>
+   <br>
+  Hey honey you could be my drug <br>
+  You could be my new prescription <br>
+  Too much could be an overdose <br>
+  All this trash talk make me itching <br>
+   <br>
+  Oh my my shit <br>
+  Everybody talks, everybody talks <br>
+  Everybody talks too much <br>
+   <br>
+  It started with a whisper <br>
+  And that was when I kissed her <br>
+  And then she made my lips hurt <br>
+  I can hear the chitchat <br>
+  Take me to your love shack <br>
+  Mamas always gotta backtrack <br>
+  When everybody talks back <br>
+   <br>
+  Never thought I'd live to see the day <br>
+  When everybody's words got in the way <br>
+   <br>
+  Hey sugar show me all your love <br>
+  All you're giving me is friction <br>
+  Hey sugar what you gotta say? <br>
+   <br>
+  It started with a whisper <br>
+  And that was when I kissed her <br>
+  And then she made my lips hurt <br>
+  I can hear the chitchat <br>
+  Take me to your love shack <br>
+  Mamas always gotta backtrack <br>
+  When everybody talks back <br>
+   <br>
+  Everybody talks <br>
+  Everybody talks <br>
+  Everybody talks <br>
+  Everybody talks <br>
+  Everybody talks <br>
+  Everybody talks... back <br>
+   <br>
+  It started with a whisper  <br>
+  (everybody talks, everybody talks) <br>
+  And that was when I kissed her  <br>
+  (everybody talks, everybody talks) <br>
+   <br>
+  Everybody talks <br>
+  Everybody talks... back <br>
+  </h3>"
+end
+
+end
